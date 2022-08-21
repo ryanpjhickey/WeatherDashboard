5a54ad9c51742471491ff8eb47a482e9d0463918
Ryan <ryanpjhickey@gmail.com>
2022-08-20 18:15:24 -0700

    commit test code with san diego coordinates and ability to fetch san diego weather information

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var api = 'https://api.openweathermap.org/data/2.5/weather?q=peshawar&appid=afafb1b6438a7da06c95113c0745c9ac'
+var api = 'https://api.openweathermap.org/data/2.5/weather?lat=32.7157&lon=-117.1611&appid=afafb1b6438a7da06c95113c0745c9ac'
 
 var searchCity = document.querySelector('#searchCity')
 var searchBtn = document.querySelector('#searchBtn')
