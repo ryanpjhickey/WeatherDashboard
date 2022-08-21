1314b7112924e5192a679778ffddb24ce828d798
Ryan <ryanpjhickey@gmail.com>
2022-08-20 18:07:58 -0700

    change api key tp have parameters that actually work. weather seems to work, onecall does not

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
-var api = 'https://api.openweathermap.org/data/2.5/onecall?appid=afafb1b6438a7da06c95113c0745c9ac'
+var api = 'https://api.openweathermap.org/data/2.5/weather?q=peshawar&appid=afafb1b6438a7da06c95113c0745c9ac'
+
 var searchCity = document.querySelector('#searchCity')
 var searchBtn = document.querySelector('#searchBtn')
 
 function callCity() {
-    if (searchCity.textContent = 'San Diego') {
-        var finalApi = api + 'lat=32.7157&lon=117.1611'
-    }
-    fetch(finalApi).then(function (response) {
+    // if (searchCity.textContent = 'San Diego') {
+    //     var finalApi = api + '&lat=32.7157&lon=117.1611'
+    // }
+    fetch(api
+    ).then(function (response) {
         if (response.ok) {
             return response.json();
         }
